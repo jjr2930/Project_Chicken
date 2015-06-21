ffe51647e6c4fc72c4d21ef7e2156e44dd76fb69
ParkChanHo <draryo@naver.com>
2015-06-21 21:49:44 +0900

    #logo

---
+++ Assets/Scripts/logofade.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class logofade : MonoBehaviour
+{
+    public GameObject 텍스쳐;
+
+    void Start()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeOut()
+    {
+        yield return new WaitForSeconds(.5f);
+        TweenAlpha.Begin(텍스쳐.gameObject, 2f, 1f);
+        yield return new WaitForSeconds(4f);
+
+        TweenAlpha.Begin(텍스쳐.gameObject, 2f, 0f);
+        yield return new WaitForSeconds(2.2f);
+
+        NextSceneCall();
+    }
+
+    void NextSceneCall()
+    {
+        Application.LoadLevel("2_GameScene");
+    }
+}
